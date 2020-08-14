fa6b93594ff9b826633b36a991a7281de8ebb19a
Harish Ponna <harishponna55@gmail.com>
2020-08-14 13:55:47 +0530

    all done

--- src/redux/states/heroState.js
+++ src/redux/states/heroState.js
@@ -4,19 +4,19 @@ const heroState = {
     "I specialize in making modern, responsive web applications & websites.",
   social_media_icons: [
     {
-      to: "#",
+      to: "mailto:harishponna55@gmail.com",
       classname: "fas fa-envelope"
     },
     {
-      to: "#",
+      to: "linkedin.com/in/harish-ponna-b9b885ba/",
       classname: "fab fa-linkedin"
     },
     {
-      to: "#",
+      to: "https://www.hackerrank.com/Harish_Ponna",
       classname: "fab fa-hackerrank"
     },
     {
-      to: "#",
+      to: "https://github.com/harish-ponna-au6/",
       classname: "fab fa-github-square"
     }
   ]
